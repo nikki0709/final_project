af2a78d61ce4b0327e8bb4acb5381ccae25f9c1f
Nikki Yuan <nikkkii@umich.edu>
2025-04-13 01:09:33 +0000

    Fix syntax error in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HTP_Test/'
+  base: '/HTP_Test/',
   plugins: [react()],
   resolve: {
     alias: {
